ea9f55f6b2ff4ce04f29aafdf31a3eeb08a7d8c1
Felipe Stahlhofer <43865622+FelipeStahl@users.noreply.github.com>
2021-05-22 15:54:35 -0300

    Empréstimo Associação/Herança

---
+++ src/main/java/br/com/model/Emprestimo.java
@@ -0,0 +1,97 @@
+package br.com.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ *
+ * @author Felipe.Stahlhofer
+ */
+@Entity
+@Table(name = "emprestimo")
+public class Emprestimo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataRetirada;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataPrevista;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataDevolucao;
+
+    public Emprestimo() {
+    }
+
+    public Emprestimo(Long id, Date dataRetirada, Date dataPrevista, Date dataDevolucao) {
+        this.dataRetirada = dataRetirada;
+        this.dataPrevista = dataPrevista;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public Date getDataRetirada() {
+        return dataRetirada;
+    }
+
+    public void setDataRetirada(Date dataRetirada) {
+        this. dataRetirada = dataRetirada;
+    }
+
+    public Date getDataPrevista() {
+        return dataPrevista;
+    }
+
+    public void setDataPrevista(Date dataPrevista) {
+        this. dataPrevista = dataPrevista;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this. dataDevolucao = dataDevolucao;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Emprestimo)) {
+            return false;
+        }
+        Emprestimo other = (Emprestimo)object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "br.com.model.Emprestimo[ id=" + id + "]";
+    }
+}
