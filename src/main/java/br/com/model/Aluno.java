e8b4c40a059816e855a410477d0cd0a5e9c6077d
Felipe Stahlhofer <43865622+FelipeStahl@users.noreply.github.com>
2021-05-22 15:54:45 -0300

    Aluno Associação/Herança

---
+++ src/main/java/br/com/model/Aluno.java
@@ -0,0 +1,33 @@
+package br.com.model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.*;
+
+/**
+ *
+ * @author Felipe.Stahlhofer
+ */
+@Entity
+@Table(name = "aluno")
+@PrimaryKeyJoinColumn(name = "idPessoa")
+public class Aluno extends Pessoa implements Serializable {
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "emprestimo", fetch = FetchType.EAGER)
+    private List<Emprestimo> emprestimo;
+
+    public Aluno() {
+    }
+
+    public Aluno(Long id, String nome, String cpf, String email, String senha, String matricula) {
+        super(id, nome, cpf, email, senha, matricula);
+    }
+    
+    public List<Emprestimo> getEmprestimo() {
+        return emprestimo;
+    }
+
+    public void setEmprestimo(List<Emprestimo> emprestimo) {
+        this.emprestimo = emprestimo;
+    }
+}
