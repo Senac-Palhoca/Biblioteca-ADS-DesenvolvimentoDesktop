db42652e7b48cc90d7ae1729b0faf91467b992d2
Felipe <arandel25@gmail.com>
2021-05-27 20:59:32 -0300

    DAO Funcionario e perfil pronto

---
+++ src/main/java/br/com/dao/FuncionarioDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.dao;
+
+import br.com.model.Funcionario;
+
+/**
+ * @author Felipe
+ */
+public interface FuncionarioDao extends BaseDao<Funcionario, Long>{
+    
+}
---
+++ src/main/java/br/com/dao/FuncionarioDaoImpl.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.dao;
+
+import br.com.model.Funcionario;
+import java.io.Serializable;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+/**
+ * @author Felipe
+ */
+public class FuncionarioDaoImpl extends BaseDaoImpl<Funcionario, Long> implements Serializable{
+
+    @Override
+    public Funcionario pesquisarPorId(Long id, Session sessao) throws HibernateException {
+        return (Funcionario) sessao.get(Funcionario.class, id);
+    }
+    
+}
