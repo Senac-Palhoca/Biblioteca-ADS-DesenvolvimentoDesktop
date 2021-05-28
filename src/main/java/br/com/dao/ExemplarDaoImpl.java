3a2e38790d1de866c583eaf544abd6ab38edcb29
marcelo <marcelo.mobile@outlook.com.br>
2021-05-28 18:59:25 -0300

    Alterado pesquisarPorLivro

--- src/main/java/br/com/dao/ExemplarDaoImpl.java
+++ src/main/java/br/com/dao/ExemplarDaoImpl.java
@@ -6,12 +6,13 @@
 package br.com.dao;
 
 import br.com.model.Exemplar;
+import br.com.model.Livro;
 import java.io.Serializable;
 import java.util.List;
 import org.hibernate.*;
 
 public class ExemplarDaoImpl extends BaseDaoImpl<Exemplar, Long> implements ExemplarDao, Serializable {
-
+    
     @Override
     public List<Exemplar> listarTodos(Session sessao) throws HibernateException {
         Query consulta = sessao.createQuery(("FROM Exemplar"));
@@ -19,15 +20,15 @@ public class ExemplarDaoImpl extends BaseDaoImpl<Exemplar, Long> implements Exem
     }
 
     @Override
-    public List<Exemplar> pesquisarPorTitulo(String titulo, Session sessao) throws HibernateException {
-        Query consulta = sessao.createQuery("from Exemplar where nome like :titulo");
-        consulta.setParameter("titulo", "%" + titulo + "%");
-        return consulta.list();
+    public Exemplar pesquisarPorId(Long id, Session sessao) throws HibernateException {
+        return (Exemplar) sessao.get(Exemplar.class, id);
     }
 
     @Override
-    public Exemplar pesquisarPorId(Long id, Session sessao) throws HibernateException {
-        return (Exemplar) sessao.get(Exemplar.class, id);
+    public List<Exemplar> pesquisarPorLivro(Livro livro, Session sessao) throws HibernateException {
+        Query consulta = sessao.createQuery("from Exemplar where nome like :titulo");
+        consulta.setParameter("titulo", "%" + livro.getTitulo() + "%");
+        return consulta.list();
     }
 
 }
